75959c6e9911df0283740cd3b97a9924ea72d5a8
Vedehi <152774479+Vedehi@users.noreply.github.com>
2026-02-14 19:39:51 +0530

    fix :  deplyment error

--- Zaryah_Frontend/app/shop/page.js
+++ Zaryah_Frontend/app/shop/page.js
@@ -1,12 +1,15 @@
 'use client'
 
+import { Suspense } from 'react'
 import { Layout } from '../components/Layout'
 import { ShopPage } from '../components/ShopPage'
 
 export default function Shop() {
   return (
     <Layout>
-      <ShopPage />
+      <Suspense fallback={<div className="min-h-screen flex items-center justify-center"><div className="animate-spin rounded-full h-12 w-12 border-4 border-primary-200 border-t-primary-600"></div></div>}>
+        <ShopPage />
+      </Suspense>
     </Layout>
   )
 }
