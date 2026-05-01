8182cb1e8857ad28c314b4c5c0989d2de0c7137b
Vedehi <152774479+Vedehi@users.noreply.github.com>
2026-05-01 14:51:44 +0530

    issue resolved

--- Zaryah_Frontend/app/login/page.js
+++ Zaryah_Frontend/app/login/page.js
@@ -1,12 +1,15 @@
 'use client'
 
+import { Suspense } from 'react'
 import { Layout } from '../components/Layout'
 import { LoginPage } from '../components/LoginPage'
 
 export default function Login() {
   return (
     <Layout>
-      <LoginPage />
+      <Suspense fallback={<div className="p-6 text-sm text-gray-500">Loading login...</div>}>
+        <LoginPage />
+      </Suspense>
     </Layout>
   )
 }
